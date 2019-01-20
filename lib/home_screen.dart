8c5c07f933429626b2984e804f4adda4fbe57aad
Tobias Reissmann <Tobias.Reissmann@telekom.de>
2019-01-20 18:07:42 +0100

    design and vibration

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
-import 'package:vibrate/vibrate.dart';  
+import 'package:vibrate/vibrate.dart';
 // import 'package:flutter/gestures.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -32,28 +32,10 @@ class _HomeScreen extends State<HomeScreen> {
           // centerTitle: true,
           title: Text(
             'QR-Shoppinglist',
-            style: TextStyle(
-                color: Colors.green, fontWeight: FontWeight.w300, fontSize: 24),
+            style: TextStyle(color: Colors.green, fontWeight: FontWeight.w300, fontSize: 24),
           ),
-          actions: _ev > 6
-              ? <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.favorite),
-                      color: Colors.red,
-                      onPressed: () {
-                        return AlertDialog(
-                            content: Text('A.L.F.'),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Icon(Icons.favorite_border),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ]);
-                      })
-                ]
-              : null,
+          actions:
+              _ev > 6 ? <Widget>[IconButton(icon: Icon(Icons.favorite), color: Colors.red, onPressed: () {})] : null,
         ),
         body: Stack(
           children: <Widget>[
@@ -67,116 +49,90 @@ class _HomeScreen extends State<HomeScreen> {
                           color: Colors.white,
                           height: MediaQuery.of(context).size.height,
                           child: ListView.builder(
-                            padding: EdgeInsets.only(top: 16),
                             itemCount: itemList.length + 1,
                             itemBuilder: (BuildContext context, int index) {
                               if (index < itemList.length) {
-                                return Card(
-                                    elevation: 5,
-                                    child: Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Row(children: <Widget>[
+                                return Padding(
+                                    padding: const EdgeInsets.only(left: 16.0, top: 8, bottom: 8, right: 8),
+                                    child: Column(
+                                      children: <Widget>[
+                                        Row(children: <Widget>[
                                           Flexible(
                                               flex: 0,
                                               child: Container(
-                                                width: MediaQuery.of(context)
-                                                        .size
-                                                        .width *
-                                                    0.6,
-                                                padding: const EdgeInsets.only(
-                                                    right: 16.0),
+                                                width: MediaQuery.of(context).size.width * 0.6,
+                                                padding: const EdgeInsets.only(right: 16.0),
                                                 child: Text(
                                                   itemList[index].name,
-                                                  style:
-                                                      TextStyle(fontSize: 20.0),
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
+                                                  style: TextStyle(fontSize: 20.0),
+                                                  overflow: TextOverflow.ellipsis,
                                                   textAlign: TextAlign.left,
                                                 ),
                                               )),
                                           Flexible(
                                               fit: FlexFit.tight,
                                               child: Container(
-                                                  child: Text(
-                                                      itemList[index].number,
-                                                      style: TextStyle(
-                                                          fontSize: 26.0),
-                                                      overflow:
-                                                          TextOverflow.ellipsis,
-                                                      textAlign:
-                                                          TextAlign.left))),
+                                                  child: Text(itemList[index].number,
+                                                      style: TextStyle(fontSize: 26.0),
+                                                      overflow: TextOverflow.ellipsis,
+                                                      textAlign: TextAlign.left))),
                                           Flexible(
                                               flex: 1,
                                               child: Container(
-                                                  alignment:
-                                                      Alignment.centerRight,
+                                                  alignment: Alignment.centerRight,
                                                   child: IconButton(
                                                       icon: Icon(Icons.cancel),
                                                       color: Colors.grey[300],
                                                       onPressed: () {
                                                         setState(() {
-                                                          this
-                                                              .itemList
-                                                              .removeAt(index);
+                                                          this.itemList.removeAt(index);
                                                         });
                                                       })))
-                                        ])));
+                                        ]),
+                                        Divider()
+                                      ],
+                                    ));
                               } else {
                                 return Column(
                                   children: <Widget>[
-                                    Card(
-                                      elevation: 5,
-                                        child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 8),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(horizontal: 8),
                                       child: Row(children: <Widget>[
                                         Flexible(
                                             flex: 0,
                                             child: Container(
-                                                width: MediaQuery.of(context)
-                                                        .size
-                                                        .width *
-                                                    0.4,
-                                                padding: EdgeInsets.symmetric(
-                                                    horizontal: 4),
+                                                width: MediaQuery.of(context).size.width * 0.4,
+                                                padding: EdgeInsets.symmetric(horizontal: 4),
                                                 child: TextFormField(
                                                   controller: mName,
                                                   style: new TextStyle(
                                                     color: Colors.black,
                                                     fontSize: 20,
                                                   ),
-                                                  keyboardType:
-                                                      TextInputType.text,
-                                                  decoration:
-                                                      const InputDecoration(
+                                                  keyboardType: TextInputType.text,
+                                                  decoration: const InputDecoration(
                                                     labelText: 'Item',
                                                   ),
                                                 ))),
                                         Flexible(
                                             flex: 0,
                                             child: Container(
-                                                width: MediaQuery.of(context)
-                                                        .size
-                                                        .width *
-                                                    0.4,
+                                                width: MediaQuery.of(context).size.width * 0.4,
                                                 child: TextFormField(
                                                   controller: mNumber,
                                                   style: new TextStyle(
                                                     color: Colors.black,
                                                     fontSize: 20,
                                                   ),
-                                                  keyboardType:
-                                                      TextInputType.number,
-                                                  decoration:
-                                                      const InputDecoration(
+                                                  keyboardType: TextInputType.number,
+                                                  decoration: const InputDecoration(
                                                     labelText: 'Number',
                                                   ),
                                                 ))),
                                         Flexible(
                                             flex: 1,
                                             child: Container(
-                                                alignment:
-                                                    Alignment.centerRight,
+                                                alignment: Alignment.centerRight,
                                                 // width: MediaQuery.of(context).size.width * 0.1,
                                                 child: IconButton(
                                                   icon: Icon(Icons.check),
@@ -184,13 +140,13 @@ class _HomeScreen extends State<HomeScreen> {
                                                   onPressed: save,
                                                 )))
                                       ]),
-                                    )),
+                                    ),
                                     Container(height: 116)
                                   ],
                                 );
                               }
                             },
-                          ))),
+                          )))
                 ]),
             showScan
                 ? Column(mainAxisAlignment: MainAxisAlignment.end, children: [
@@ -203,15 +159,12 @@ class _HomeScreen extends State<HomeScreen> {
                               height: 100,
                               buttonColor: Colors.white,
                               child: RaisedButton(
+                                  elevation: 8,
                                   textColor: Colors.green,
                                   onPressed: scan,
-                                  child: const Text('SCAN',
-                                      style: TextStyle(
-                                          fontSize: 32.0,
-                                          fontWeight: FontWeight.w300)),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(20))))),
+                                  child:
+                                      const Text('SCAN', style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w300)),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))))),
                     )),
                   ])
                 : Container(),
@@ -221,8 +174,7 @@ class _HomeScreen extends State<HomeScreen> {
 
   Future scan() async {
     try {
-      print(await Vibrate.canVibrate);
-      Vibrate.feedback(FeedbackType.light);
+      Vibrate.feedback(FeedbackType.selection);
       String scan = await BarcodeScanner.scan();
       RegExp expNumber = new RegExp(r"([0-9])\w+");
       RegExp expNameKg = new RegExp(r"^.*\skg\s");
@@ -231,17 +183,13 @@ class _HomeScreen extends State<HomeScreen> {
       RegExp expValid = new RegExp(r"^VG\s([0-9]{3,4})");
       if (expValid.hasMatch(scan)) {
         if (expNameKg.hasMatch(scan)) {
-          setState(() => this
-              .itemList
-              .add(Item(scan.split(" kg ")[1], expNumber.stringMatch(scan))));
+          setState(() => this.itemList.add(Item(scan.split(" kg ")[1], expNumber.stringMatch(scan))));
         } else {
           if (expNameBund.hasMatch(scan)) {
-            setState(() => this.itemList.add(
-                Item(scan.split(" Bund ")[1], expNumber.stringMatch(scan))));
+            setState(() => this.itemList.add(Item(scan.split(" Bund ")[1], expNumber.stringMatch(scan))));
           } else {
             if (expNameStueck.hasMatch(scan)) {
-              setState(() => this.itemList.add(
-                  Item(scan.split(" Stück ")[1], expNumber.stringMatch(scan))));
+              setState(() => this.itemList.add(Item(scan.split(" Stück ")[1], expNumber.stringMatch(scan))));
             }
           }
         }
