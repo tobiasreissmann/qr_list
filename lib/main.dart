fd9768e49a30e3dbcd1f4d4cb6b8c249952bbc30
tobiasreissmann <tobias.reissmann@hotmail.com>
2019-02-21 23:56:34 +0100

    design refinement

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:qr_list/gui/qrList.dart';
 import 'package:qr_list/themes.dart';
 
 main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.white));
   runApp(QRListApp());
 }
 
