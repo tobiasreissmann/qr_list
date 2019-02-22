196f52691f9e7697470cea9dbcceb0eb2696b83f
tobiasreissmann <tobias.reissmann@hotmail.com>
2019-02-22 22:24:38 +0100

    persitency fix
    from gitHub issue #6

--- lib/services/database.service.dart
+++ lib/services/database.service.dart
@@ -10,12 +10,12 @@ Future<Database> get database async {
 
 Future<List<Item>> get databaseItemList async {
   String path = join(await getDatabasesPath(), 'items.db');
-  Database database = await openDatabase(path, version: 1, onCreate: (Database db, int version) async => await db.execute('CREATE TABLE Items (name TEXT, number TEXT PRIMARY KEY)'));
-  List<Map> list = await database.rawQuery('SELECT * FROM Items');
+  Database database = await openDatabase(path,
+      version: 1, onCreate: (Database db, int version) async => await db.execute('CREATE TABLE Items (name TEXT, number TEXT PRIMARY KEY)'));
+  List<Map> _list = await database.rawQuery('SELECT * FROM Items');
   List<Item> _itemList = [];
-  for (var i = 0; i < list.length; i++) _itemList.add(Item(list[i]['name'], list[i]['number']));
-  // await database.close();
-  return _itemList;  
+  _list.forEach((entry) => _itemList.add(Item(entry['name'], entry['number'])));
+  return _itemList;
 }
 
 void databaseDeleteTable() async {
@@ -23,9 +23,18 @@ void databaseDeleteTable() async {
   await _database.transaction((txn) async => await txn.rawInsert('DELETE FROM Items'));
 }
 
-void databaseDeleteItem(Item item) async {
+void databaseAddItem(Item item) async {
   Database _database = await database;
   await _database.transaction((txn) async => await txn.rawInsert('INSERT INTO Items(name, number) VALUES("${item.name}", "${item.number}")'));
+}
+
+void databaseSaveItemList(List<Item> itemList) async {
+  Database _database = await database;
+  await _database.transaction((txn) async => await txn.rawInsert('DELETE FROM Items'));
+  itemList.forEach(
+    (item) async =>
+        await _database.transaction((txn) async => await txn.rawInsert('INSERT INTO Items(name, number) VALUES("${item.name}", "${item.number}")')),
+  );
 }
 
 void databaseRemoveItem(String number) async {
